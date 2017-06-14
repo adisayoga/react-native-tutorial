ba6859fa6a97987630d80ab279c854feaf39c0af
Adi Sayoga <adisayoga@gmail.com>
2017-06-14 16:40:27 +0800

    Justify content.

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import { View } from 'react-native';
 export default class App extends Component {
   render() {
     return (
-      // Try setting `flexDirection` to `column`.
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      // Try setting `justifyContent` to `center`.
+      // Try setting `flexDirection` to `row`.
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
